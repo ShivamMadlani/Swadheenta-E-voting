8212e154e058b702b218fcda58e9eee7c2f63cfd
unknown <harshmetkel24@gmail.com>
2023-02-04 22:44:05 +0530

    routers fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Web3 from "web3";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
-import '../CSS/App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "../CSS/App.css";
 
 // component and pages import
 import Home from "../pages/Home";
@@ -26,14 +26,9 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/vote" element={<UserLogin />}></Route>
+        <Route path="/login" element={<UserLogin />}></Route>
       </Routes>
     </BrowserRouter>
-    // <div>
-    //   <div>
-    //     <Home />
-    //   </div>
-    // </div>
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,12 @@ import "../CSS/Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">D-मतदान</div>
-      <Link className="vote-btn" to='/vote'>Proceed to vote</Link>
+      <Link to="/" className="logo">
+        D-मतदान
+      </Link>
+      <Link className="vote-btn" to="/login">
+        Proceed to vote
+      </Link>
     </nav>
   );
 };
