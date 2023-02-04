77dc4fc20822567b4d3efd1c2681f0486e02c52a
unknown <harshmetkel24@gmail.com>
2023-02-04 22:21:56 +0530

    images added to landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,29 @@ const Home = () => {
       <div className="home-page">
         <Navbar />
         <div className="home-container">
-            
+          <div className="box">
+            <img
+              className="box-img"
+              src={process.env.PUBLIC_URL + "/images/decentralised.png"}
+              alt=""
+            />
+          </div>
+
+          <div className="box">
+            <img
+              className="box-img"
+              src={process.env.PUBLIC_URL + "/images/ethereum.jpg"}
+              alt=""
+            />
+          </div>
+
+          <div className="box">
+            <img
+              className="box-img"
+              src={process.env.PUBLIC_URL + "/images/e-voting.jpg"}
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </>
